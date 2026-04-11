d0b3a3b9945a833275fef5ebd7b63d34f5650382
JavohirBoboxonov <javohirboboxonov08@gmail.com>
2026-04-11 15:26:21 +0500

    some bugs added

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('users.urls')),
@@ -14,4 +14,9 @@ urlpatterns = [
     path('notifications/', include('notifications.urls')),
     path('payments/', include('paymets.urls')),
     path('salary/', include('salary.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Swagger UI interfeysi
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    # Redoc (muqobil interfeys)
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
